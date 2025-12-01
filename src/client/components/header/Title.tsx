575053a8c6815f5c98741ee82f3320f042bd41c6
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2025-11-30 19:20:58 -0500

    Correcting name

--- src/client/components/header/Title.tsx
+++ src/client/components/header/Title.tsx
@@ -4,7 +4,7 @@ import '../../../App.css';
 function Title() {
   return (
     <div className="title-container">
-      <h2 className="title-text">Tutorrrr Learner Database</h2>
+      <h2 className="title-text">Tutor Learner Database</h2>
     </div>
   );
 }
